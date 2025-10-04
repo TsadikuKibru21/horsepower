ba4b0d46f9a360e0c195bdd4b68060e3048fd5c1
Tsadiku21 <tsadikuk@gmail.com>
2025-10-04 09:26:09 +0300

    dashboard

---
+++ chart_sample/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    "name":"Chart js",
+    "depends":[
+        "base","web","account"
+    ],
+    "data":[
+        "views/chart_menu.xml"
+    ],
+    "assets": {
+        "web.assets_backend": [
+            
+            "chart_sample/static/src/js/main.js",
+            "chart_sample/static/src/js/main.sale_dashboard.js",
+            "chart_sample/static/src/js/chart_sample.js",
+            "chart_sample/static/src/js/sales_dashboard.js",
+            "chart_sample/static/src/js/kpi_card.js",
+            "chart_sample/static/src/xml/kpi_card.xml",
+            "chart_sample/static/src/xml/purchase.xml",
+            "chart_sample/static/src/js/chart_renderer.js",
+            "chart_sample/static/src/xml/chart_smaple.xml",
+            "chart_sample/static/src/scss/sales_dashboard.xml",
+            
+            
+
+        ]
+    }
+}
