dd40e73795c3ecaeb1e003b6bdc4e24c32f1c921
Tsadiku21 <tsadikuk@gmail.com>
2025-10-11 08:48:18 +0300

    update

--- custom_sales_report/models/sale.py
+++ custom_sales_report/models/sale.py
@@ -10,7 +10,7 @@ class SaleOrder(models.Model):
  
 class SaleOrderLine(models.Model):
     _inherit="sale.order.line"
-    commission_percent = fields.Float(string='Commission Percent',related="product_id.commission_percent" ,help='Commission percentage for this product')
+    commission_percent = fields.Float(string='Commission Percent',related="product_id.commission_percent", readonly=False,help='Commission percentage for this product')
 
 
 class ProductProduct(models.Model):
